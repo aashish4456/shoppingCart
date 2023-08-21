64b5a3eae480263b64096c84c90759adda878444
aashish4456 <aashishapatel838686@gmail.com>
2023-08-21 15:19:44 +0530

    ...

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let shop=document.getElementById('shop');
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+ 
 let shopItemsData = [
     {
       id: "jfhgbvnscs",
@@ -32,6 +34,7 @@ let shopItemsData = [
   let generateShop =()=>{
      return (shop.innerHTML=shopItemsData.map((x)=>{
         let { id, name, price, desc, img } = x;
+        let search = basket.find((x) => x.id === id) || [];
     return `
     <div id=product-id-${id} class="item">
     <img width="220" src=${img} alt="">
@@ -42,7 +45,9 @@ let shopItemsData = [
         <h2>$ ${price} </h2>
         <div class="buttons">
           <i onclick="decrement(${id})" class="bi bi-dash-lg" ></i>
-          <div id=${id} class="quantity">0</div>
+          <div id=${id} class="quantity">
+          ${search.item === undefined ? 0 : search.item}
+          </div>
           <i onclick="increment(${id})" class="bi bi-plus-lg" ></i>
         </div>
       </div>
@@ -51,11 +56,9 @@ let shopItemsData = [
     
   }).join(""));
 
-};  
+}; 
+console.log(Array.isArray(basket)); 
 generateShop();
-let basket =[{
-
-}];
 
 
 let increment=(id)=>{
@@ -70,8 +73,9 @@ let increment=(id)=>{
 }
 else{
       search.item+=1;
-}  
-   update(selectItem.id);
+}
+update(selectItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
  let decrement=(id)=>{
     let selectItem=id;
@@ -83,9 +87,19 @@ return;
 else{
       search.item-=1;
 }  
-update(selectItem.id); 
+update(selectItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+ 
+  localStorage.setItem("data", JSON.stringify(basket));
+  
  };
  let update=(id)=>{
     let search=basket.find((x)=>x.id===id);
     document.getElementById(id).innerHTML=search.item;
+    calculation();
  }
+ let calculation = ()=>{
+      let cartIcon=document.getElementById('cartAmount');
+     cartIcon.innerHTML=basket.map((x) => x.item).reduce((x,y)=>x+y,0);
+ }
+ calculation();  
